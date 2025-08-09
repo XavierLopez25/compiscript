d424f19124a80832f1892416359774d656443e64
XavierLopez25 <lop22716@uvg.edu.gt>
2025-08-08 21:37:10 -0600

    feat(driver): run semantic analysis and export AST to DOT

--- program/Driver.py
+++ program/Driver.py
@@ -2,13 +2,28 @@ import sys
 from antlr4 import *
 from CompiscriptLexer import CompiscriptLexer
 from CompiscriptParser import CompiscriptParser
+from SemanticVisitor import SemanticVisitor
+from AST.symbol_table import SemanticError
+
+
+from AST.ast_to_dot import write_dot
 
 def main(argv):
     input_stream = FileStream(argv[1])
     lexer = CompiscriptLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = CompiscriptParser(stream)
-    tree = parser.program()  # We are using 'prog' since this is the starting rule based on our Compiscript grammar, yay!
+    tree = parser.program()
+
+    sem = SemanticVisitor()
+    try:
+        ast = sem.visit(tree)
+        print("Semantic analysis completed successfully.")
+        write_dot(ast, "ast.dot")
+        print("AST -> ast.dot (usa: dot -Tpng ast.dot -o ast.png)")
+
+    except SemanticError as e:
+        print(f"Semantic error: {e}")
 
 if __name__ == '__main__':
     main(sys.argv)
